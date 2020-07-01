2ea04c1e8182d1e069aa4f24ed40ea16786ce7ef
TEEN-BOOM <51919967+TEEN-BOOM@users.noreply.github.com>
2020-07-01 21:17:49 +0530

    Misleading documentation

--- bootstrap.js
+++ bootstrap.js
@@ -39,20 +39,21 @@ child.stderr.setEncoding('utf8')
 
 child.stdout.on('data', (data) => {
     console.log(data);
+    logger.log(data);
 });
 
 child.stderr.on('data', (data) => {
     console.error(data);
+    logger.error(data);
 });
 
 child.on('error', (error) => {
     console.error(error);
+    logger.error(error);
 });
 
 child.on('close', (code) => {
     console.log(`Lavalink exited with code ${code}`);
+    logger.log(`Lavalink exited with code ${code}`);
 });
-logger.setSync(async logs => {
-//     logs.forEach(log => console.log(log));
-    return logs;
-})
+
