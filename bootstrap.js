c29115999cb3c2ca67022845b34dea16e5f84b78
TEEN-BOOM <51919967+TEEN-BOOM@users.noreply.github.com>
2020-07-02 00:09:53 +0530

    REvert

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,5 @@
 
 const fs = require('fs')
-const { Timber } = require("@timberio/node");
 let application = fs.readFileSync('./application.yml', 'utf8')
 
 if (process.env.PORT) {
@@ -10,18 +9,6 @@ if (process.env.PORT) {
 if (process.env.PASS) {
     application = application.replace('youshallnotpass', process.env.PASS)
 }
-
-var ID = process.env.ID
-var KEY = process.env.KEY
-
-var logger = new Timber(ID, KEY, {
-  syncMax: 10,
-  ignoreExceptions: false
-});
-// logger.setSync(async logs => {
-//     logs.forEach(log => console.log(log));
-//     return logs;
-// })
 
 fs.writeFileSync('./application.yml', application)
 
@@ -33,21 +20,17 @@ child.stderr.setEncoding('utf8')
 
 child.stdout.on('data', (data) => {
     console.log(data);
-    logger.log(data);
 });
 
 child.stderr.on('data', (data) => {
     console.error(data);
-    logger.error(data);
 });
 
 child.on('error', (error) => {
     console.error(error);
-    logger.error(error);
 });
 
 child.on('close', (code) => {
     console.log(`Lavalink exited with code ${code}`);
-    logger.log(`Lavalink exited with code ${code}`);
 });
 
